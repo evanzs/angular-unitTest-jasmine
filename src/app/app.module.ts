02b177dc14b340ecfe08d62cc152ab43ccbe775a
evanzs <evandrofidk@hotmail.com>
2023-03-06 13:20:30 -0300

    refactor(app.module)::hammer: remove router configs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { CalcComponent } from './calc/calc.component';
 
@@ -12,7 +12,7 @@ import { CalcComponent } from './calc/calc.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
