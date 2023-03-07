c1c5e4a72cfb57ab42d3ae0002fd60dd3e835ad5
evanzs <evandrofidk@hotmail.com>
2023-03-07 11:16:33 -0300

    feat(calc.component)::sparkles: add functions setSacar and setDepositar with get values

--- src/app/calc/calc.component.ts
+++ src/app/calc/calc.component.ts
@@ -1,10 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-calc',
   templateUrl: './calc.component.html',
   styleUrls: ['./calc.component.css']
 })
-export class CalcComponent {
+export class CalcComponent implements OnInit {
+  ngOnInit(): void {
+
+  }
+  private  poupanca = 10;
+  private  carteira = 10;
+  private  montante = 10;
+
+  setSacar(value:string):number | null{
+    let sacarValue = Number(value);
+    if(isNaN(sacarValue))
+       return null;
+
+    this.carteira += parseInt(value);
+    this.poupanca -= parseInt(value);
+    return sacarValue;
+  }
+
+  public setDepositar(value:string):number | null{
+
+    let depositValue = Number(value);
+    if(isNaN(depositValue))
+       return null;
+
+    this.carteira -= parseInt(value);
+    this.poupanca += parseInt(value);
+    return depositValue;
+  }
+
+  get getMontante():number{
+    return this.poupanca + this.carteira;
+  }
+
+  get getPoupanca():number{
+    return this.poupanca
+  }
+
+  get getCarteira():number{
+    return this.carteira
+  }
+
 
 }
