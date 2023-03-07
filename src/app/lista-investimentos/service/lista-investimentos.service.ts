6f8574dd231879354f18a8f9525cfc8da06c9620
evanzs <evandrofidk@hotmail.com>
2023-03-07 15:47:02 -0300

    feat(lista-investimentos.service)::sparkles: create list-investiment service

--- src/app/lista-investimentos/service/lista-investimentos.service.ts
+++ src/app/lista-investimentos/service/lista-investimentos.service.ts
@@ -1,9 +1,16 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { map, Observable } from 'rxjs';
+import { Investimento } from 'src/app/investimento';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ListaInvestimentosService {
+  private URL:string = "https://raw.githubusercontent.com/troquatte/fake-server/main/investiments-all.json";
+  constructor( private http:HttpClient) { }
 
-  constructor() { }
+  public list():Observable<Array<Investimento>>{
+    return this.http.get<Array<Investimento>>(this.URL).pipe(map(res=>res));
+  }
 }
