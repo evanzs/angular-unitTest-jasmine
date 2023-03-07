6de7f1809a55754695e0b86cdedb4d470f1a64d2
evanzs <evandrofidk@hotmail.com>
2023-03-07 15:19:14 -0300

    refactor(app.component)::hammer: import lista-investimentos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { CalcComponent } from './calc/calc.component';
+import { ListaInvestimentosComponent } from './lista-investimentos/lista-investimentos.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    CalcComponent
+    CalcComponent,
+    ListaInvestimentosComponent
   ],
   imports: [
     BrowserModule,
