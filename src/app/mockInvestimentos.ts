f6c469abc24f0f9db078a1c652607393f443b7fc
evanzs <evandrofidk@hotmail.com>
2023-03-07 15:17:32 -0300

    feat(mockINvestimentos)::sparkles: create mockInvestimentos list

---
+++ src/app/mockInvestimentos.ts
@@ -0,0 +1,9 @@
+import { Investimento } from "./investimento";
+
+
+export const INVESTIMENTOS:Array<Investimento> =[
+  {name:'Banco 1', value:100},
+  {name:'Banco 2', value:100},
+  {name:'Banco 3', value:100},
+  {name:'Banco 4', value:100}
+]
