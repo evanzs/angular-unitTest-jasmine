13a2c27e1248c48121f9a85e607891666f8e4915
evanzs <evandrofidk@hotmail.com>
2023-03-07 11:59:48 -0300

    test(calc.component)::rotating_light: change describe name

--- src/app/calc/tests/interface/calc.component.spec.ts
+++ src/app/calc/tests/interface/calc.component.spec.ts
@@ -3,7 +3,7 @@ import { CalcComponent } from '../../calc.component';
 
 
 
-describe('Interface CalcComponent', () => {
+describe('interface CalcComponent', () => {
   let component: CalcComponent;
   let nativeComponent: any;
   let fixture: ComponentFixture<CalcComponent>;
@@ -21,7 +21,29 @@ describe('Interface CalcComponent', () => {
   });
 
   it('should contain Montante value', () => {
-    expect(nativeComponent.querySelector('#montante').textContent).toContain(component.getMontante);
+    expect(nativeComponent.querySelector('#h_montante').textContent).toContain("Montante: RS "+component.getMontante);
   });
+
+  it('should contain Carteira value', () => {
+    expect(nativeComponent.querySelector('#h_carteira').textContent).toContain("Carteira: RS "+component.getCarteira);
+  });
+
+  it('should contain Poupanca value', () => {
+    expect(nativeComponent.querySelector('#h_poupanca').textContent).toContain("Poupança: RS "+component.getPoupanca);
+  });
+
+  it('should contain Sacar on btnSacar', () => {
+    expect(nativeComponent.querySelector('#btn_sacar').textContent).toContain("Sacar");
+  });
+
+  it('should contain Depositar on btnDepositar', () => {
+    expect(nativeComponent.querySelector('#btn_depositar').textContent).toContain("Depositar");
+  })
+
+  it('should transfer carteira to poupanca', () => {
+
+     nativeComponent.querySelector('#')
+    expect(nativeComponent.querySelector('#btnDepositar').textContent).toContain("Depositar");
+  })
 
 });
