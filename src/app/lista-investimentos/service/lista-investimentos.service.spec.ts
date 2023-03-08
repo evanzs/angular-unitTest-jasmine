f476c10b9c1db3d5e68e81fd785ab8c23e081ebf
evanzs <evandrofidk@hotmail.com>
2023-03-07 23:35:16 -0300

    feat(lista-investiment.service)::sparkles: config options to test

--- src/app/lista-investimentos/service/lista-investimentos.service.spec.ts
+++ src/app/lista-investimentos/service/lista-investimentos.service.spec.ts
@@ -1,12 +1,18 @@
+import { HttpClient } from '@angular/common/http';
 import { TestBed } from '@angular/core/testing';
 
 import { ListaInvestimentosService } from './lista-investimentos.service';
-
+import {HttpClientTestingModule, HttpTestingController} from '@angular/common/http/testing'
 describe('ListaInvestimentosService', () => {
   let service: ListaInvestimentosService;
-
+   let httpTestingController:HttpTestingController;
+   let httpClient:HttpClient;
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports:[HttpClientTestingModule],
+    });
+    httpClient = TestBed.inject(HttpClient);
+    httpTestingController = TestBed.inject(HttpTestingController)
     service = TestBed.inject(ListaInvestimentosService);
   });
 
