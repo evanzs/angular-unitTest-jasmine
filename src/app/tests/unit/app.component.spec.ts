c4184be970717d53e0d82e6aa3dd4a17e9777cf6
evanzs <evandrofidk@hotmail.com>
2023-03-23 11:11:10 -0300

    test:rotating_light: add new imports

--- src/app/tests/unit/app.component.spec.ts
+++ src/app/tests/unit/app.component.spec.ts
@@ -2,6 +2,8 @@ import { CalcComponent } from './../../calc/calc.component';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { AppComponent } from '../../app.component';
 import { ListaInvestimentosComponent } from 'src/app/lista-investimentos/lista-investimentos.component';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { MatCardModule } from '@angular/material/card';
 
 
 describe('appComponent',()=>{
@@ -10,7 +12,8 @@ describe('appComponent',()=>{
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations:[AppComponent,CalcComponent,ListaInvestimentosComponent]
+      declarations:[AppComponent,CalcComponent,ListaInvestimentosComponent],
+      imports:[HttpClientTestingModule, MatCardModule],
     })
     .compileComponents();
 
